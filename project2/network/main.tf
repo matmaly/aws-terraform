b5ac647c602a7df00c9b115f364058820b7d56b6
matmaly <mateuszfmaly@gmail.com>
2020-05-10 13:08:43 +0000

    Adding public route table and its association to the internet gateway

--- project2/network/main.tf
+++ project2/network/main.tf
@@ -16,7 +16,7 @@ resource "aws_internet_gateway" "internet_gateway_tf" {
   ]
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_tf" {
   vpc_id     		  = "${aws_vpc.vpc_tf.id}"
   cidr_block 		  = "${var.public_subnet_cidr}"
   map_public_ip_on_launch = true
@@ -25,10 +25,30 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet_tf" {
   vpc_id     = "${aws_vpc.vpc_tf.id}"
   cidr_block = "${var.private_subnet_cidr}"
   tags = {
     Name = "private subnet"
   }
 }
+
+resource "aws_route_table" "public_route_table_tf" {
+  vpc_id = "${aws_vpc.vpc_tf.id}"
+  route {
+    cidr_block  = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.internet_gateway_tf.id}" 
+  }
+  tags = {
+    Name = "public_route_table_tf"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_association_tf" {
+  subnet_id 	 = aws_subnet.public_subnet_tf.id
+  route_table_id = aws_route_table.public_route_table_tf.id 
+}
+
+
+
+
